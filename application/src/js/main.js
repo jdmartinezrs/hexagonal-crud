4e32b121b15178a561cc1a4073813cfe29152ab4
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-09-12 03:14:58 -0500

    feat: :construction: Getting the cookie in the web console

---
+++ application/src/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    let config = {
+        method: e.target.method,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data) // Fixed typo here
+    };
+    try {
+        let response = await fetch(e.target.action, config);
+        let result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
